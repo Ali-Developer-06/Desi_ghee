0035b19895ccd403b5dfbec75a8014f3ca7304ca
Ali-Developer-06 <alideveloper066@gmail.com>
2024-01-02 20:50:14 +0500

    website update

--- script.js
+++ script.js
@@ -16,3 +16,16 @@ let prevScrollPos = window.pageYOffset;
     function closePopUp(){
         popUp.classList.remove('open-popUp');
     }
+
+    const hamburger = document.querySelector('.hamburger');
+    const navMenu = document.querySelector('.nav-menu');
+    
+    hamburger.addEventListener('click', ()=>{
+        hamburger.classList.toggle('active');
+        navMenu.classList.toggle('active');
+    })
+    
+    document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', ()=>{
+        hamburger.classList.remove('active');
+        navMenu.classList.remove('active');
+    }))
